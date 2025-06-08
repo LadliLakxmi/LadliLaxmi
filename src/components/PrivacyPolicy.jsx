21c7a4d4a3b2560c64d1e638894daf0e854c51bc
LadliLakxmi <ladlilaxmi22@gmail.com>
2025-06-08 09:11:36 +0530

    Update PrivacyPolicy.jsx

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import LegalDoc from "../assets/LegalDocument.jpg"
+import Panimg from "../assets/PanRegistration.jpg"
 
 const PrivacyPolicy = () => {
   return (
-    <div>PrivacyPolicy</div>
+    <div className='flex flex-col items-center justify-center'>
+      <p className='text-4xl p-4 border-b rounded-md '>
+        PrivacyPolicy
+        </p>
+        <div className='m-2'> 
+          <img className="mb-2  rounded-md" src={LegalDoc} height="800px" width={"800px"} alt="Legal Documents" srcset="" />
+          <img className="mb-2  rounded-md" src={Panimg} height="800px" width={"800px"} alt="Legal Documents" srcset="" />
+        </div>
+
+    </div>
+    
   )
 }
 
