a3ef53e3ab6e5c0c3e01b6f430a24e581dbe7a5b
LadliLakxmi <ladlilaxmi22@gmail.com>
2025-06-12 14:57:57 +0530

    Update DashboardOverview.jsx

--- src/components/Profile/DashboardOverview.jsx
+++ src/components/Profile/DashboardOverview.jsx
@@ -187,14 +187,17 @@ const DashboardOverview = ({ user }) => {
   ];
 
   return (
-    <div className="p-4 sm:p-6 lg:p-10 bg-gradient-to-br from-gray-50 to-gray-200 rounded-3xl shadow-2xl w-full max-w-7xl mx-auto border border-gray-100">
+    <div className="p-4 sm:p-6 lg:p-10 bg-gradient-to-br mt-8 md:mt-2 from-gray-50 to-gray-200 rounded-3xl shadow-2xl w-full max-w-7xl mx-auto border border-gray-100">
       {/* Welcome Section */}
       <div className="mb-8 text-center">
         <h2 className="text-3xl sm:text-4xl lg:text-5xl -z-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-600 to-yellow-800 mb-2 drop-shadow-md">
           Welcome, {user.name?.split(" ")[0] || "User"}!
         </h2>
+        <p className="text-gray-700 text-md sm:text-lg  font-medium">
+          Email: {user.email}
+        </p>
         <p className="text-gray-700 text-md sm:text-lg mb-4 font-medium">
-          {user.email}
+          Phone: {user.phone}
         </p>
       </div>
 
