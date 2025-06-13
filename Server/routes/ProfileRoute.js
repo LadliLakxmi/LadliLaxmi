9ae31d4b6661c1ab6eb9f2ede80763c9a089909a
LadliLakxmi <ladlilaxmi22@gmail.com>
2025-06-13 11:29:14 +0530

    Update ProfileRoute.js

--- Server/routes/ProfileRoute.js
+++ Server/routes/ProfileRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { auth } = require("../middleware/auth");
-const { getProfile,updateBankDetails } = require("../controllers/profile");
+const { getProfile,updateBankDetails,updateProfile } = require("../controllers/profile");
 // const { protect } = require("../middleware/auth"); // You must have this middleware
 const router = express.Router();
 
 // router.get("/getprofile", protect,getProfile); // Ensure you have the correct method from your controller
 router.get("/getprofile/:id",auth, getProfile); // ✅ Fixed route
 router.put('/bank-details',auth, updateBankDetails);
+router.put('/update-profile',auth, updateProfile);
 module.exports = router;
