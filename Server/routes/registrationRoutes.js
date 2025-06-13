7ba88f2a679e363a20bf2c33f9bbbfb6ca4c0784
LadliLakxmi <ladlilaxmi22@gmail.com>
2025-06-13 11:29:33 +0530

    Update registrationRoutes.js

--- Server/routes/registrationRoutes.js
+++ Server/routes/registrationRoutes.js
@@ -1,12 +1,13 @@
 const {auth } = require("../middleware/auth")
 const express = require("express");
-const {register , logout ,login } = require("../controllers/Registration")
+const {register , logout ,login,changePassword } = require("../controllers/Registration")
 const {contactUsController} = require("../controllers/ContactUs.js")
 const router = express.Router();
 
 router.post('/register',register);
 router.post('/login',login);
 router.post('/logout',logout);
+router.post('/changepassword',auth,changePassword);
  //import controller
 
 //mapping 
