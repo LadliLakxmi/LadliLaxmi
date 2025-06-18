e348104aacfd7f70a2dc29aa646de0bc09f0553b
LadliLakxmi <ladlilaxmi22@gmail.com>
2025-06-18 13:25:39 +0530

    Update walletTransactionRoutes.js

--- Server/routes/walletTransactionRoutes.js
+++ Server/routes/walletTransactionRoutes.js
@@ -9,6 +9,7 @@ const {
   deleteWalletTransaction,
   getTransactionsForUser,
 } = require('../controllers/walletTransactionController');
+const {transferSponsorToMain} =require("../controllers/moneytransfer")
 
 const router = express.Router();
 // // Apply auth middleware to all routes in this router
@@ -27,5 +28,5 @@ router.route('/:id')
   // New route for user-specific transactions
 router.route('/user/:userId') // Endpoint will be e.g., /api/wallet-transactions/user/60d0fe4f5311236168a109ca
   .get(getTransactionsForUser);
-
+router.route('/transferToMain/:userId').post(transferSponsorToMain)
 module.exports = router;
