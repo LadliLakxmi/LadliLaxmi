eea2fd03ad3f5af7f94c88b7c54ddd14251bb5ec
LadliLakxmi <ladlilaxmi22@gmail.com>
2025-06-18 13:37:16 +0530

    Update TransferSponsorToMain.jsx

--- src/components/Profile/TransferSponsorToMain.jsx
+++ src/components/Profile/TransferSponsorToMain.jsx
@@ -24,7 +24,7 @@ const TransferSponsorToMain = ({ user }) => {
     try {
       setLoading(true);
       const response = await axios.post(
-        `http://localhost:4001/api/v1/wallet-transactions/transferToMain/${userId}`,
+        `https://ladlilaxmi.onrender.com/api/v1/wallet-transactions/transferToMain/${userId}`,
         {
           amount: Number(amount),
         }
