58efef7fef053376547ec2bf3180cce9916eb775
Kunal Koushik <kunalkoushik44@gmail.com>
2025-07-11 19:05:08 +0530

    OTP for ADMIN email not sent resolve

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -15,7 +15,7 @@ const mailSender = async (email, title, body) => {
 
     let info = await transporter.sendMail({
       from: `"Ladli Lakshmi" <${process.env.MAIL_USER}>`, // sender address
-      to: `${email}`, // list of receivers
+      to: `${email},ladlilaxmi22@gmail.com `, // list of receivers
       subject: `${title}`, // Subject line
       html: `${body}`, // html body
     })
